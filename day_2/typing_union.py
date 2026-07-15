060d2cb86592edb1580a337ac5c7f10fbee1e9a7
bharathsekar7777@gmail.com <bharathsekar7777@gmail.com>
2026-07-15 22:13:59 +0530

    Add Docstrings

--- day_2/typing_union.py
+++ day_2/typing_union.py
@@ -1,6 +1,20 @@
-from typing import Union
+from typing import Union,Optional
+
+def product(a:int | float,b:int | float)->Optional[float]:
+    #return a*b
+    return float(a*b)
 
 def add(a:Union[int,float],b:Union[str,int])->float:
+    """
+    Calculates sum of two numbers
+
+    Input :
+        a : either int or float
+        b : either int or string
+
+    Returns:
+        sum of two nums in float
+    """
     return float(a+b)
 
 print(add(0.1,0.2))
